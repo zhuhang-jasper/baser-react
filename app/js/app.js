8e73a5da3e796f56f10621d05a412cf8c9e78d8b
Blaine Schmeisser <blainesch@gmail.com>
2017-10-18 11:03:46 -0700

    Update the tools.

--- app/js/app.js
+++ app/js/app.js
@@ -7,18 +7,42 @@ class Board extends Component {
     super(props)
     this.state = {
       board: Array(13).fill().map(() => {
-        return Array(10).fill().map(() => '')
+        return Array(10).fill().map(() => ({}))
       })
     }
   }
 
-  handleValue = ({ key, target }) => {
+  handleValue = ({ target }) => {
     const { row, column } = target.dataset
+    const { value } = target
+
     this.setState({
       board: Array(13).fill().map((_, rowIndex) => {
         return Array(10).fill().map((_, columnIndex) => {
           if (row == rowIndex && column == columnIndex) {
-            return key.toUpperCase()
+            return {
+              mode: this.state.board[rowIndex][columnIndex].mode,
+              letter: value.toUpperCase()
+            }
+          }
+          return this.state.board[rowIndex][columnIndex]
+        })
+      })
+    })
+  }
+
+  handleMode = ({ target }) => {
+    const { mode } = this.props
+    const { row, column } = target.dataset
+
+    this.setState({
+      board: Array(13).fill().map((_, rowIndex) => {
+        return Array(10).fill().map((_, columnIndex) => {
+          if (row == rowIndex && column == columnIndex) {
+            return {
+              mode,
+              letter: this.state.board[rowIndex][columnIndex].letter,
+            }
           }
           return this.state.board[rowIndex][columnIndex]
         })
@@ -33,8 +57,9 @@ class Board extends Component {
           { this.state.board.map((row, rowIndex) => (
             <li key={rowIndex}>
               <ul>
-                { row.map((letter, columnIndex) => {
+                { row.map(({ letter, mode }, columnIndex) => {
                   const classes = classnames({
+                    [mode]: mode,
                     invalid: letter && !letter.match(/^[a-z]$/i),
                   })
                   return <li key={columnIndex}>
@@ -43,7 +68,8 @@ class Board extends Component {
                       data-row={rowIndex}
                       data-column={columnIndex}
                       value={letter}
-                      onKeyPress={this.handleValue} />
+                      onFocus={this.handleMode}
+                      onChange={this.handleValue} />
                   </li>
                 }) }
               </ul>
@@ -66,35 +92,72 @@ const ForkMe = () => (
 )
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      mode: 'typing',
+    }
+  }
+
+  onModeChange = (e) => {
+    const mode = e.target.id
+    this.setState({
+      mode,
+    })
+  }
+
   render() {
+    const { mode } = this.state
+
     return (
       <div>
         <ForkMe />
         <h2>Wordbase Helper</h2>
-        <Board />
+        <Board mode={mode} />
         <div className="tools">
           <h3>Tools</h3>
           <ul>
             <li>
-              <input type="radio" />
+              <input
+                id="typing"
+                type="radio"
+                name="mode"
+                defaultChecked={this.state.mode === 'typing'}
+                onChange={this.onModeChange} />
               <label htmlFor="typing">Typing</label>
             </li>
             <li>
-              <input type="radio" />
+              <input
+                id="orange"
+                type="radio"
+                name="mode"
+                defaultChecked={this.state.mode === 'orange'}
+                onChange={this.onModeChange} />
               <label htmlFor="orange">Opponent Color</label>
             </li>
             <li>
-              <input type="radio" name="tool" id="blue" value="blue"/>
+              <input
+                id="blue"
+                type="radio"
+                name="mode"
+                defaultChecked={this.state.mode === 'blue'}
+                onChange={this.onModeChange} />
               <label htmlFor="blue">My Color</label>
             </li>
             <li>
-              <input type="radio" name="tool" id="clear" value="clear"/>
+              <input
+                id="clear"
+                type="radio"
+                name="mode"
+                defaultChecked={this.state.mode === 'clear'}
+                onChange={this.onModeChange} />
               <label htmlFor="clear">Clear Color</label>
             </li>
           </ul>
         </div>
         <div className="answers">
-          <h3><span>Word List</span>
+          <h3>
+            <span>Word List</span>
             <select onChange={this.orderWords}>
               <option value="Length">Length</option>
               <option value="Highest">Highest</option>
