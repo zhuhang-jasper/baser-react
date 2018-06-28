2740e66087542d310e7379bc8a3a03f7f72a6528
zhuhang-jasper <zhuhang.jasper@gmail.com>
2018-06-28 23:02:54 +0800

    add trusted dictionary as default

--- app/js/lib/dictionary.js
+++ app/js/lib/dictionary.js
@@ -1,5 +1,6 @@
 import trie from 'trie'
-import words from '../../static/ospd4.txt'
+import words from '../../static/dicts.txt'
+//import words from '../../static/ospd4.txt'
 
 var dictionary = trie.createTrieFromArray(words.split('\n'));
 
