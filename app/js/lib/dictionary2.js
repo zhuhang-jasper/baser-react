818d5088dc65e900baa52ff56e6b55be48cff7e8
zhuhang-jasper <zhuhang.jasper@gmail.com>
2018-06-28 23:02:08 +0800

    rename dict2

--- app/js/lib/dictionary2.js
+++ app/js/lib/dictionary2.js
@@ -1,6 +1,6 @@
 import trie from 'trie'
-import words from '../../static/combined.txt'
-//import words2 from '../../static/words_alpha.txt'
+import words from '../../static/unchecked.txt'
+//import words from '../../static/words_alpha.txt'
 
 var dictionary2 = trie.createTrieFromArray(words.split('\n'));
 
