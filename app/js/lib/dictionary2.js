7b9d69255dfb4245568fbdacef4b322a671af684
zhuhang-jasper <zhuhang.jasper@gmail.com>
2018-06-28 17:03:54 +0800

    try new dictionary

--- app/js/lib/dictionary2.js
+++ app/js/lib/dictionary2.js
@@ -1,6 +1,7 @@
 import trie from 'trie'
-import words2 from '../../static/words_alpha.txt'
+import words from '../../static/combined.txt'
+//import words2 from '../../static/words_alpha.txt'
 
-var dictionary2 = trie.createTrieFromArray(words2.split('\n'));
+var dictionary2 = trie.createTrieFromArray(words.split('\n'));
 
 export { dictionary2 }
